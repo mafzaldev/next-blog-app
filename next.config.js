b11b32ae74858325d0dd53f4750ea9b6a2b78300
Afzal <afzalashraf814@gmail.com>
2022-12-19 20:14:07 +0500

    Final commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ["images.unsplash.com", "thrangra.sirv.com"],
+  },
   reactStrictMode: true,
-}
+  swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
